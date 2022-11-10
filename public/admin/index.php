ee1987798e94964926665d01755380c06173d994
sergio <sergio.chulian@iesdonana.org>
2022-11-10 21:12:45 +0100

    tailwind

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,13 +1,17 @@
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="es">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="/dist/output.css" rel="stylesheet">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado de artículos</title>
 </head>
 <body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
     <?php
     require '../comunes/auxiliar.php';
     require 'comunes/auxiliar.php';
